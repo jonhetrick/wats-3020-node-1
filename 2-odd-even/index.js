07758012c120f86e5c7c360e5b2d64f104f9093e
jonhetrick <jhetrick@seattleu.edu>
2020-01-20 00:20:29 -0800

    Finished odd-even

--- 2-odd-even/index.js
+++ 2-odd-even/index.js
@@ -3,21 +3,22 @@ The user should run this from the root of the project using `node 1-odd-even/ind
 */
 
 // get an integer using getargs
-let getargs = require("../modules/getargs/index.js");
+let getargs = require('../modules/getargs/index.js');
 let input = getargs.getIntegerArg();
 
 /**** Do not modify anything above this line ****/
 
 // test that input is a number
-// TODO use the if statement to check input using isNaN and NOT Number.isInteger 
-if () {
-  // TODO use console.log to provide a usage statement when user forgets to enter a number
+// TODO use the if statement to check input using isNaN and NOT Number.isInteger
+if (isNaN(input) || !Number.isInteger(input)) {
+	console.log('usage: node 2-odd-even <integer>');
 } else {
-  // TODO use the if statement to test if integer is divisible by 2
-  if () {
-    //TODO use console.log with a template literal to report that the number is even
-
-  } else {
-    //TODO use console.log with a template literal to report that the number is odd
-  }
+	// TODO use the if statement to test if integer is divisible by 2
+	if (input % 2 == 0) {
+		//TODO use console.log with a template literal to report that the number is even
+		console.log(`${input} is even!`);
+	} else {
+		//TODO use console.log with a template literal to report that the number is odd
+		console.log(`${input} is odd!`);
+	}
 }
